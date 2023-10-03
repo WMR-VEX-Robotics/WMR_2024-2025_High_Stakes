ce88505127b0894022da8407bf5dc09ac288929e
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2023-10-03 15:28:57 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,18 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       bowma                                                     */
-/*    Created:      10/2/2023, 8:41:19 PM                                     */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
 #include "vex.h"
 
 using namespace vex;
-
-// A global instance of competition
 competition Competition;
 
-// define your global instances of motors and other devices here
-// motors
-motor tlMotor1 = motor(PORT1, ratio6_1, true);
-motor blMotor11 = motor(PORT11, ratio6_1, true);
-motor trMotor10 = motor(PORT10, ratio6_1, false);
-motor brMotor20 = motor(PORT20, ratio6_1, false);
-motor catapaultMotor = motor(PORT5, ratio36_1, true);
+/*---------------------------------------------------------------------------*/
+/*                             VEXcode Config                                */
+/*                                                                           */
+/*  Before you do anything else, start by configuring your motors and        */
+/*  sensors using the V5 port icon in the top right of the screen. Doing     */
+/*  so will update robot-config.cpp and robot-config.h automatically, so     */
+/*  you don't have to. Ensure that your motors are reversed properly. For    */
+/*  the drive, spinning all motors forward should drive the robot forward.   */
+/*---------------------------------------------------------------------------*/
 
-// not motors
-controller mainController = controller(primary);
-digital_out wingPneumatics = digital_out(Brain.ThreeWirePort.A);
-
-//odometry
 /*---------------------------------------------------------------------------*/
 /*                             JAR-Template Config                           */
 /*                                                                           */
@@ -46,13 +32,13 @@ ZERO_TRACKER_NO_ODOM,
 //You will input whatever motor names you chose when you configured your robot using the sidebar configurer, they don't have to be "Motor1" and "Motor2".
 
 //Left Motors:
-motor_group(tlMotor1, blMotor11),
+motor_group(),
 
 //Right Motors:
-motor_group(trMotor10, brMotor20),
+motor_group(),
 
 //Specify the PORT NUMBER of your inertial sensor, in PORT format (i.e. "PORT1", not simply "1"):
-PORT8,
+PORT1,
 
 //Input your wheel diameter. (4" omnis are actually closer to 4.125"):
 3.25,
@@ -110,37 +96,77 @@ PORT3,     -PORT4,
 int current_auton_selection = 0;
 bool auto_started = false;
 
-
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
-
 void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  default_constants();
 
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+  while(auto_started == false){            //Changing the names below will only change their names on the
+    Brain.Screen.clearScreen();            //brain screen for auton selection.
+    switch(current_auton_selection){       //Tap the brain screen to cycle through autons.
+      case 0:
+        Brain.Screen.printAt(50, 50, "Drive Test");
+        break;
+      case 1:
+        Brain.Screen.printAt(50, 50, "Drive Test");
+        break;
+      case 2:
+        Brain.Screen.printAt(50, 50, "Turn Test");
+        break;
+      case 3:
+        Brain.Screen.printAt(50, 50, "Swing Test");
+        break;
+      case 4:
+        Brain.Screen.printAt(50, 50, "Full Test");
+        break;
+      case 5:
+        Brain.Screen.printAt(50, 50, "Odom Test");
+        break;
+      case 6:
+        Brain.Screen.printAt(50, 50, "Tank Odom Test");
+        break;
+      case 7:
+        Brain.Screen.printAt(50, 50, "Holonomic Odom Test");
+        break;
+    }
+    if(Brain.Screen.pressing()){
+      while(Brain.Screen.pressing()) {}
+      current_auton_selection ++;
+    } else if (current_auton_selection == 8){
+      current_auton_selection = 0;
+    }
+    task::sleep(10);
+  }
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+  auto_started = true;
+  switch(current_auton_selection){  
+    case 0:
+      drive_test(); //This is the default auton, if you don't select from the brain.
+      break;        //Change these to be your own auton functions in order to use the auton selector.
+    case 1:         //Tap the screen to cycle through autons.
+      drive_test();
+      break;
+    case 2:
+      turn_test();
+      break;
+    case 3:
+      swing_test();
+      break;
+    case 4:
+      full_test();
+      break;
+    case 5:
+      odom_test();
+      break;
+    case 6:
+      tank_odom_test();
+      break;
+    case 7:
+      holonomic_odom_test();
+      break;
+ }
 }
 
 /*---------------------------------------------------------------------------*/
@@ -152,13 +178,6 @@ void autonomous(void) {
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
-
-void tankDrive_user(){
-  tlMotor1.spin(forward, mainController.Axis2.position(), percent);
-  trMotor10.spin(forward, mainController.Axis3.position(), percent);
-  blMotor11.spin(forward, mainController.Axis2.position(), percent);
-  brMotor20.spin(forward, mainController.Axis3.position(), percent);
-}
 
 void usercontrol(void) {
   // User control code here, inside the loop
@@ -172,6 +191,10 @@ void usercontrol(void) {
     // update your motors, etc.
     // ........................................................................
 
+    //Replace this line with chassis.control_tank(); for tank drive 
+    //or chassis.control_holonomic(); for holo drive.
+    chassis.control_arcade();
+
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
@@ -182,7 +205,7 @@ void usercontrol(void) {
 //
 int main() {
   // Set up callbacks for autonomous and driver control periods.
- Competition.autonomous(autonomous);
+  Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
 
   // Run the pre-autonomous function.
@@ -190,8 +213,6 @@ int main() {
 
   // Prevent main from exiting with an infinite loop.
   while (true) {
-    
+    wait(100, msec);
   }
-
-
 }
