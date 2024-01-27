ae936516e840d1f6d9763294aeea9844ae5e4b8d
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2024-01-27 11:08:50 -0500

    h

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ motor tlMotor17 = motor(PORT18, ratio6_1, true);
 motor mlMotor19 = motor(PORT19, ratio6_1, true);
 motor blMotor20 = motor(PORT20, ratio6_1, true);
 motor trMotor13 = motor(PORT16, ratio6_1, false);
-motor mrMotor12 = motor(PORT5, ratio6_1, false);
+motor mrMotor12 = motor(PORT7, ratio6_1, false);
 motor brMotor11 = motor(PORT14, ratio6_1, false);
 /*motor tlMotor17 = motor(PORT17, ratio18_1, true);
 motor mlMotor19 = motor(PORT19, ratio18_1, true);
@@ -529,7 +529,7 @@ void competitionAutonL(){
   chassis.turn_to_angle(270);
 
   chassis.drive_distance(-10);
-
+ 
   chassis.turn_to_angle(225);
 
   chassis.drive_distance(18);
@@ -968,6 +968,6 @@ int main() {
 
   // Run the pre-autonomous function.
   pre_auton();
-  assess();
+  //assess();
   //autonType(type);
 }
