47c1ac2a1b0a8da6be9c7e224d3af4b51f1e82f5
Anthony Santsevich <26_asantsevich@wmrhsd.int>
2024-11-21 16:42:39 -0500

    Can't get wall stake motor to run

--- src/main.cpp
+++ src/main.cpp
@@ -268,6 +268,27 @@ void red_positive_auton(){
 
 }
 
+void skillsAuton(){
+  //
+  solonoidB.open();
+  MotorGroupWS.spin(vex::directionType::rev,100,percent);//get wall stake to go backward
+  //chassis.drive_distance(-6);
+  //ring has to slide forward or start in that climb thing
+  wait(0.5,sec);
+  /*chassis.drive_distance(4);
+  chassis.turn_to_angle(-90);
+  chassis.drive_distance(-7);
+  solonoidA.close();
+  chassis.turn_to_angle(180);
+  intake_motor.spin(forward,12.5, volt);
+  hook_motor.spin(forward,12.5, volt);
+  chassis.drive_distance(14);*/
+
+
+  
+
+}
+
 
 void autonomous(void) {
   //blue_negative_auton();
@@ -277,6 +298,8 @@ void autonomous(void) {
   //blueleftside
 
   //red_positive_auton();
+
+  skillsAuton();
 }
 
 /*---------------------------------------------------------------------------*/
